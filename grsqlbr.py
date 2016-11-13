da1820f8ac032c660e1327a18016d3f64f12aecf
alex <alex@pdp7.net>
2016-11-13 19:36:33 +0100

    Parse target properly

--- grsqlbr.py
+++ grsqlbr.py
@@ -16,17 +16,21 @@ def render():
     assert flask.request.form['format'] == 'json'
     return flask.json.jsonify(get_points(flask.request.form['from'],
                                          flask.request.form['until'],
-                                         flask.request.form['target'],
+                                         flask.request.form.getlist('target'),
                                          flask.request.form['maxDataPoints'],
                                          ))
 
 
 def get_metrics():
-    return [{'is_leaf': 1, 'name': 'dummy', 'path': 'foo'}]
+    return [
+        {'is_leaf': 1, 'name': 'foo', 'path': 'foo.'},
+        {'is_leaf': 1, 'name': 'bar', 'path': 'foo.'},
+        {'is_leaf': 0, 'name': 'foo', 'path': ''},
+    ]
 
 
-def get_points(from_, until, target, max_data_points):
-    return [{'target': 'entries', 'datapoints': random_data_points()}]
+def get_points(from_, until, targets, max_data_points):
+    return [{'target': target, 'datapoints': random_data_points()} for target in targets]
 
 
 def random_data_points():
